fb2f7a5d954d17d3bc6e05dddc587b417e27b6c8
taehyun <gtaehyun0825@gmail.com>
2025-04-06 12:55:54 +0900

    Make Item

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type { Ammo, Coin, Grenade, Heart, Weapon };
+    public Type type;
+    public int value;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+    }
+}
